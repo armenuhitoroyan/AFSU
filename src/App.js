8c52247d4df75c48bd328c80753f725bc65c7208
armenuhitoroyan <armenuhi.h.toroyan@gmail.com>
2024-11-19 21:35:26 +0400

    added  timer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import "./App.css";
-// import Footercomponent from "./Components/Footer/FooterComponent";
+import React, { useState, useEffect } from "react";
+import Footercomponent from "./Components/Footer/FooterComponent";
 
-// import Menu from "./Components/Header/Header";
+import Menu from "./Components/Header/Header";
 import Loader from "./Components/Other/Loader";
 // import Home3 from "./Components/Homepages/Home3";
-// import Home1 from "./Components/Homepages/Home";
+import Home1 from "./Components/Homepages/Home";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Set a timer
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    // Clean up the timer
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      {/* <Menu /> */}
-      {/* <Home1 /> */}
-      <Loader />
-      {/* <Footercomponent /> */}
+      <div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div>
+            <Menu />
+            <Home1 />
+            <Footercomponent />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
