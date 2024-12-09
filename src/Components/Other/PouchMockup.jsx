e08c563e93bbb2bd16cb4bbd4c2d6a85c6a209ce
armenuhitoroyan <armenuhi.h.toroyan@gmail.com>
2024-12-10 02:55:54 +0400

    implemented image zoom

--- src/Components/Other/PouchMockup.jsx
+++ src/Components/Other/PouchMockup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../style/PouchMockup.css"; // External CSS file for styles
 
 const PouchMockup = ({ src, width, height }) => {
   return (
@@ -7,44 +8,24 @@ const PouchMockup = ({ src, width, height }) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: { width },
-        height: { height },
+        width: width,
+        height: height,
         borderRadius: "15px",
+        overflow: "hidden",
+        position: "relative",
       }}
+      className="pouch-container"
     >
-      <div
+      <img
+        src={src}
+        alt="Paper Pouch Mockup"
         style={{
           width: "100%",
           height: "100%",
-          overflow: "hidden",
-          borderRadius: "15px",
-          position: "relative",
-          transition: "transform 0.5s ease, box-shadow 0.5s ease",
+          objectFit: "cover",
         }}
-        className="pouch-container"
-      >
-        <img
-          src={src}
-          alt="Paper Pouch Mockup"
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            borderRadius: "15px",
-            transition: "transform 0.5s ease",
-          }}
-          className="pouch-image"
-        />
-      </div>
-
-      <style>
-        {`
-          .pouch-container:hover {
-            transform: scale(1.1) rotate(45deg);
-            box-shadow: 0px 15px 25px rgba(0, 0, 0, 0.3);
-          }
-        `}
-      </style>
+        className="pouch-image"
+      />
     </div>
   );
 };
